181c76d577905427e3413a53339b77b3f4565f7b
m0dusX <31633375+m0dusX@users.noreply.github.com>
2017-10-08 22:19:34 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,8 @@
-import sys
-import string
 import re
 import argparse
 from getpass import getpass
+import sys
+import string
 
 
 def make_badlist(badlist):
@@ -10,36 +10,36 @@ def make_badlist(badlist):
         return bad_pass_txt.readlines()
 
 
-def regex_check(password):
-    symbol_rating = 0
+def symbol_strength_check(password):
+    symbol_strength = 0
     password_minimum = 8
     if len(password) >= password_minimum:
-        symbol_rating += 2
+        symbol_strength += 2
     if not password.isdigit():
         if re.search(r'[a-z]', password) and re.search(r'[A-Z]', password):
-            symbol_rating += 2
+            symbol_strength += 2
         if re.search(r'\d+', password):
-            symbol_rating += 1
+            symbol_strength += 1
         if any(char in string.punctuation for char in password):
-            symbol_rating += 2
-    return symbol_rating
+            symbol_strength += 2
+    return symbol_strength
 
 
-def additional_checks(password, personal_data, bad_passes):
-    additional_rating = 0
+def predictable_substrings_check(password, personal_data, bad_passes):
+    no_predictable_substrings = 0
     if personal_data['username'].lower() not in password.lower() and \
             personal_data['date_of_birth'] not in password:
-        additional_rating += 2
+        no_predictable_substrings += 2
     bad_pass_list = [bad_pass.strip() for bad_pass in bad_passes]
     if not any(bad_pass in password for bad_pass in bad_pass_list):
-        additional_rating += 1
-    return additional_rating
+        no_predictable_substrings += 1
+    return no_predictable_substrings
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Password strength checker')
-    parser.add_argument('path_to_badlist',
-                        help='path to txt file with bad passwords')
+    parser = argparse.ArgumentParser(description="Password strength checker")
+    parser.add_argument("path_to_badlist",
+                        help="path to txt file with bad passwords")
     args = parser.parse_args()
     badlist = args.path_to_badlist
     personal_data = {}
@@ -53,6 +53,7 @@ if __name__ == '__main__':
     else:
         personal_data['date_of_birth'] = date_of_birth
     password = getpass(prompt='Please enter your password: ')
-    rank = regex_check(password) + \
-        additional_checks(password, personal_data, bad_passes)
+    rank = symbol_strength_check(password) + \
+        predictable_substrings_check(password, personal_data, bad_passes)
     print('Your password rating: {}'.format(rank))
+    sys.exit()
